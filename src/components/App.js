642d6e88a2376e097a3aed6e518ee4b0deeb2b6a
Romain <romainyvernes@gmail.com>
2021-10-27 16:30:31 -0700

    Set up router, add main components

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,58 @@
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import axios from 'axios';
+
+// stylesheet
 import '../styles/App.css';
 
+// components
+import Profile from './Profile';
+import Login from './auth/Login';
+import Friends from './Friends';
+import Newsfeed from './Newsfeed';
+import Navbar from './layout/Navbar';
+
 function App() {
+  const [user, setUser] = useState({
+    isLoggedIn: false,
+    username: null
+  });
+
+  const handleRegistration = () => {
+    axios.post('/api/auth/register', {})
+  };
+
+  const handleAuthentication = () => {};
+
+  const getUserProfile = () => {
+    axios.get('')
+  };
+
+  const getFriends = () => {
+
+  };
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        {
+          /* display navigation bar only if user is authenticated, as it doesn't
+          appear on sign in page */
+          user.isLoggedIn && <Navbar />
+        }
+        <Route exact path="/" component={() => {
+          // display user's newsfeed only if authenticated
+          if (user.isLoggedIn) return <Newsfeed />;
+          return <Login />;
+        }} />
+        <Route exact path="/:username" component={(props) => {
+          // pass in props to be able to access "match" property that contains
+          // the URI's parameter
+          <Profile {...props} />
+        }} />
+        <Route exact path="/friends" component={Friends} />
+      </div>
+    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
-import Routes from './components/Routes';
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Routes />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
