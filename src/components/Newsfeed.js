78b663c400a8b4b508c9346d9f9bc5632092a0d4
Romain <romainyvernes@gmail.com>
2021-11-01 18:54:20 -0700

    Add Newsfeed component basic setup

--- src/components/Newsfeed.js
+++ src/components/Newsfeed.js
@@ -1,11 +1,47 @@
-import React from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import axios from 'axios';
 
-const Newsfeed = () => {
+// component to display recent posts by user's friends
+function Newsfeed({ auth }) {
+  const [posts, setPosts] = useState([]);
+  
+  // retrieve list of friends for authenticated user from API upon mounting
+  useEffect(() => {
+    axios.get(`/api/posts?authorId=`).then((response) => {
+      setPosts();
+    }).catch((err) => {
+      console.log(err.response.data);
+    });
+  }, []);
+
   return (
     <div>
-      
+      <ul>
+        {posts.map((post) => {
+          return (
+            <li key={post._id}>
+              <h6>{post.author}</h6>
+              <time dateTime={post.date}>{post.date}</time>
+              <p>{post.content}</p>
+            </li>
+          );
+        })}
+        </ul>
     </div>
-  )
+  );
+}
+
+Newsfeed.propTypes = {
+  auth: PropTypes.object.isRequired
 };
 
-export default Newsfeed;
+const mapStateToProps = (state) => {
+  return {
+    auth: state.auth
+  };
+};
+
+export default connect(mapStateToProps)(Newsfeed);
